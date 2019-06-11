277188cc6fe90d8478320aae7a7468b6e79b6c2a
Hasan SEZER <vimevim@users.noreply.github.com>
2019-06-11 14:27:45 +0300

    logo eklendi

--- SlimDXExamples/GamePadMouse.Designer.cs
+++ SlimDXExamples/GamePadMouse.Designer.cs
@@ -29,18 +29,27 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GamePadMouse));
             this.mouseTimer = new System.Windows.Forms.Timer(this.components);
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
             this.SuspendLayout();
             // 
             // mouseTimer
             // 
             this.mouseTimer.Tick += new System.EventHandler(this.mouseTimer_Tick);
             // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
             // GamePadMouse
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "GamePadMouse";
             this.Text = "Mouse";
             this.ResumeLayout(false);
@@ -50,5 +59,6 @@
         #endregion
 
         private System.Windows.Forms.Timer mouseTimer;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
     }
 }
